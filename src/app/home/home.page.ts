68574ac4070c0aeed136df2cc1c2800aa6554408
Iury Lazoski <lazoski@gmail.com>
2020-05-30 23:56:38 -0300

    pokedex com pokemons

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../services/pokemon.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  offset = 0;
+  pokemon = [];
 
+  constructor(private pokemonService: PokemonService) {}
+
+  ngOnInit() {
+    this.loadPokemon();
+  }
+
+  loadPokemon() {
+    this.pokemonService.getPokemon(this.offset).subscribe(res => {
+      console.log(res);
+      this.pokemon = res;
+    });
+  }
 }
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
+  baseUrl = 'https://pokeapi.co/api/v2'
+  imageUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPokemon(offset = 0) {
+    return this.http.get(`${this.baseUrl}/pokemon?offset=${offset}&limit=25`)
+      .pipe(
+        map(res => {
+          return res['results'];
+        }),
+        map(pokemons => {
+          return pokemons.map((poke, index) => {
+            poke.index = index + offset + 1;
+            poke.image = this.getPokemonImageUrl(poke.index);
+            return poke;
+          })
+        })
+      );
+  }
+
+  getPokemonImageUrl(index) {
+    return `${this.imageUrl}${index}.png`;
+  }
 }
