d19b4d598c52f19fa1f2d221e57c98a5c2160595
Iury Lazoski <lazoski@gmail.com>
2020-05-31 00:03:15 -0300

    infinite scroll for pokemon

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { PokemonService } from '../services/pokemon.service';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -11,16 +12,30 @@ export class HomePage implements OnInit {
   offset = 0;
   pokemon = [];
 
+  @ViewChild(IonInfiniteScroll)
+  infinite: IonInfiniteScroll;
+
   constructor(private pokemonService: PokemonService) {}
 
   ngOnInit() {
     this.loadPokemon();
   }
 
-  loadPokemon() {
+  loadPokemon(loadMore = false, event?) {
+    if (loadMore) {
+      this.offset += 25;
+    }
     this.pokemonService.getPokemon(this.offset).subscribe(res => {
       console.log(res);
-      this.pokemon = res;
+      this.pokemon = [...this.pokemon, ...res];
+
+      if (event) {
+        event.target.complete();
+      }
+
+      if (this.offset == 125) {
+        this.infinite.disabled = true;
+      }
     });
   }
 }
